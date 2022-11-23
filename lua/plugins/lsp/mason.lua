fd819c8542037d7e3b22973c9818f98fdbe051c4
brehen <mkluften93@gmail.com>
2022-11-23 12:02:25 +0100

    🚚 Move plugins to lua folder and cleanup

---
+++ lua/plugins/lsp/mason.lua
@@ -0,0 +1,62 @@
+local status, mason = pcall(require, "mason")
+if not status then
+	return
+end
+local status2, mason_lsp = pcall(require, "mason-lspconfig")
+if not status2 then
+	return
+end
+local mason_null_ls_status, mason_null_ls = pcall(require, "mason-null-ls")
+if not mason_null_ls_status then
+	return
+end
+
+local config = {
+	-- disable virtual text
+	virtual_text = false,
+	update_in_insert = true,
+	underline = true,
+	severity_sort = true,
+	float = {
+		focusable = true,
+		style = "minimal",
+		border = "rounded",
+		source = "always",
+		header = "",
+		prefix = "",
+		-- width = 40,
+	},
+}
+
+vim.diagnostic.config(config)
+mason.setup()
+
+mason_lsp.setup({
+	-- list of servers for mason to install
+	ensure_installed = {
+		"clangd",
+		"dockerls",
+		"sumneko_lua",
+		"pyright",
+		"rust_analyzer",
+		"taplo",
+		"jsonls",
+	},
+	-- auto-install configured servers (with lspconfig)
+	automatic_installation = true, -- not the same as ensure_installed
+})
+
+mason_null_ls.setup({
+	-- list of formatters & linters for mason to install
+	ensure_installed = {
+		"prettier", -- ts/js formatter
+		"stylua", -- lua formatter
+		"eslint_d", -- ts/js linter
+		"pylint", -- ts/js linter
+		"black",
+		"flake8",
+		"isort",
+	},
+	-- auto-install configured formatters & linters (with null-ls)
+	automatic_installation = true,
+})
