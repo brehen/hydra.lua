d13cac142faa63a607360809bfa185101eebb5b2
brehen <mkluften93@gmail.com>
2022-12-04 14:33:47 +0100

    🎨 Alias opt

--- lua/brehen/base.lua
+++ lua/brehen/base.lua
@@ -1,41 +1,43 @@
 vim.cmd("autocmd!")
 
+local opt = vim.opt
+
 vim.scriptencoding = "utf-8"
-vim.opt.encoding = "utf-8"
-vim.opt.fileencoding = "utf-8"
+opt.encoding = "utf-8"
+opt.fileencoding = "utf-8"
 
 vim.wo.number = true
 
-vim.opt.autoindent = true -- New
-vim.opt.backspace = { "start", "eol", "indent" } -- New
-vim.opt.backup = false -- New
-vim.opt.backupskip = { "/tmp/*", "/private/tmp/*" } -- New
-vim.opt.breakindent = true
-vim.opt.cmdheight = 1 -- New
-vim.opt.expandtab = true -- New
+opt.autoindent = true -- New
+opt.backspace = { "start", "eol", "indent" } -- New
+opt.backup = false -- New
+opt.backupskip = { "/tmp/*", "/private/tmp/*" } -- New
+opt.breakindent = true
+opt.cmdheight = 1 -- New
+opt.expandtab = true -- New
 -- Set highlight on search
-vim.opt.hlsearch = false
+opt.hlsearch = false
 -- Case insensitive searching UNLESS /C or capital in search
-vim.opt.ignorecase = true
-vim.opt.inccommand = "split" -- New
-vim.opt.laststatus = 2 -- New
+opt.ignorecase = true
+opt.inccommand = "split" -- New
+opt.laststatus = 2 -- New
 -- Finding files - Search down into subfolders
-vim.opt.path:append({ "**" }) -- New
-vim.opt.scrolloff = 10 -- New
+opt.path:append({ "**" }) -- New
+opt.scrolloff = 10 -- New
 -- Set terminal in nvim to fish
-vim.opt.shell = "fish" -- New, OK
-vim.opt.shiftwidth = 2
-vim.opt.showcmd = true -- New
-vim.opt.smartcase = true
-vim.opt.smartindent = true -- New
-vim.opt.smarttab = true -- New
-vim.opt.tabstop = 2
-vim.opt.title = true -- New
-vim.opt.undofile = true
-vim.opt.termguicolors = true
-vim.opt.wildignore:append({ "*/node_modules/*" }) -- New
+opt.shell = "fish" -- New, OK
+opt.shiftwidth = 2
+opt.showcmd = true -- New
+opt.smartcase = true
+opt.smartindent = true -- New
+opt.smarttab = true -- New
+opt.tabstop = 2
+opt.title = true -- New
+opt.undofile = true
+opt.termguicolors = true
+opt.wildignore:append({ "*/node_modules/*" }) -- New
 -- No Wrap lines
-vim.opt.wrap = false -- New
+opt.wrap = false -- New
 
 -- Undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
@@ -48,7 +50,7 @@ vim.api.nvim_create_autocmd("InsertLeave", {
 })
 
 -- Add asterisks in block comments
-vim.opt.formatoptions:append({ "r" })
+opt.formatoptions:append({ "r" })
 
 -- Ray so options
 vim.g.ray_options = "colors=breeze&padding=64"
