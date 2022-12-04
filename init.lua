d40fa467088f0cef31efb27dad609c86d422cf7e
brehen <mkluften93@gmail.com>
2022-12-04 14:22:04 +0100

    ✨ Nv chad variant

--- init.lua
+++ init.lua
@@ -1,15 +1,34 @@
-require("brehen.base")
-require("brehen.highlights")
-require("brehen.maps")
-require("brehen.plugins")
+vim.defer_fn(function()
+  pcall(require, "impatient")
+end, 0)
 
-local has = vim.fn.has
-local is_mac = has("macunix")
-local is_win = has("win32")
+require "core"
+require "core.options"
 
-if is_mac then
-  require("brehen.macos")
+-- setup packer + plugins
+local fn = vim.fn
+local install_path = fn.stdpath "data" .. "/site/pack/packer/opt/packer.nvim"
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e222a" })
+  print "Cloning packer .."
+  fn.system { "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path }
+
+  -- install plugins + compile their configs
+  vim.cmd "packadd packer.nvim"
+  require "plugins"
+  vim.cmd "PackerSync"
+
+  -- install binaries from mason.nvim & tsparsers
+  vim.api.nvim_create_autocmd("User", {
+    pattern = "PackerComplete",
+    callback = function()
+      vim.cmd "bw | silent! MasonInstallAll" -- close packer window
+      require("packer").loader "nvim-treesitter"
+    end,
+  })
 end
-if is_win then
-  require("brehen.windows")
-end
+
+pcall(require, "custom")
+
+require("core.utils").load_mappings()
